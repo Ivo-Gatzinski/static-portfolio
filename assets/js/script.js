a6f41373efa74fde53279074635f4d14b74f1bb5
Ezhko <ucsd.camp.21@gmail.com>
2021-10-10 11:24:27 -0700

    add more links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
 $(".surf").click(function() {
     window.open("https://ivo-gatzinski.github.io/weather-dash");
 });
+
+$(".LED").click(function() {
+    window.open("https://ivo-gatzinski.github.io/workday-planner");
+});
+
+$(".calc").click(function() {
+    window.open("https://ivo-gatzinski.github.io/password-generator");
+});
+
+$(".run").click(function() {
+    window.open("https://reeeeeahn.github.io/adoption-connection/");
+});
+
